f9729dfe67bd63754f42a5dd0a3cbbe61884f7bd
user <kurdybaniuk.serhii@gmail.com>
2023-08-16 17:22:09 +0100

    init commit

---
+++ lesson1/hw1js.js
@@ -0,0 +1,34 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+let q = 'hello'
+let w = 'owu'
+let e = 'com'
+let r = 'ua'
+let t = 1
+let y = 10
+let u = -999
+let i = 123
+let o = 3.14
+let p = 2.7
+let z = 16
+let s = true
+let d = false
+
+console.log(q,w,e,r,t,y,u,i,o,p,z,s,d,)
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+let firstName = 'Serhii '
+let middleName = 'Vasylovych '
+let lastName = 'Kurdybaniuk '
+
+let person = firstName + middleName + lastName
+console.log(person)
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+    let a = 100; let b = '100'; let c = true;
+console.log(typeof a,b,c)
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+let firstName1 = prompt('Serhii')
+let middleName1 = prompt('Vasylovych')
+let lastName1 = prompt('Kurdybaniuk')
